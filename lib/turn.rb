e5932ffe4fcadfe3c7fb9aa2d1e3d5fe2197f6b1
S Yoon <sohizzhang@gmail.com>
2019-06-14 22:46:54 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -56,7 +56,7 @@ def turn(board)
     input = gets.strip
     index = input_to_index(input)
     break if valid_move?(board, index)
-  end   
+  end
 
   move(board, index, value="X")
   display_board(board)
